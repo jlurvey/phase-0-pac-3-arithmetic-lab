5c4a0fdf59b09019f5b6a5fe9b4afc78fb6c6e13
Joseph Lurvey <joeylurvey@gmail.com>
2023-04-04 22:12:32 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function increment(n) {
+    n += 1
+    return n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+function makeInt(n) {
+    n = parseInt(n, 10);
+    return n;
+}
+
+function preserveDecimal(n) {
+    n = parseFloat(n);
+    return n;
+}
+
+
+
+
+console.log(parseInt('0x2328',10));
+
